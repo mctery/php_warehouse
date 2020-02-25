ccb3c350cfb2d3bd438010f7810567cc157d559a
Thanawat Lerdlumyong <tlerdlumyong@live.com>
2020-02-25 14:12:13 +0700

    edit order_type
    - edit sql

--- login/orders_type.php
+++ login/orders_type.php
@@ -20,39 +20,36 @@
          <?php require 'side_bar.php';?>
         <div class="col bg-light">
           <div class="card bg-light" style="width:100%; height: auto; margin:10px 0 10px 0;">
-                <table class="table" style="margin:0px;">
-                  <thead>
+            <table class="table" style="margin:0px;">
+              <thead>
+              <tr>
+                  <td>รหัสรายการสินค้า</td>
+                  <td colspan="2">ชื่อสินค้า</td>
+                  <td>จำนวนสินค้า</td>
+                  <td>ราคาสินค้า</td>
+                  <td>วันที่</td>
+                  <td>ยืนยันโดย (User)</td>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                $sql_product = "SELECT * FROM orders,orders_type,product
+                WHERE orders.orders_type_id = orders_type.orders_type_id AND orders.product_id = product.product_id AND orders.orders_type_id = '{$_REQUEST['o_id']}'";
+                //echo $sql_product;
+                $query_sql_product_type = mysqli_query($connect,$sql_product);
+                while ($row = $query_sql_product_type->fetch_assoc()) { ?>
                   <tr>
-                      <td>รหัสรายการสินค้า</td>
-                      <td>ประเภทรายการ</td>
-                      <td>จำนวนสินค้า</td>
-                      <td>ราคาสินค้า</td>
-                      <td>วันที่</td>
-                      <td>ชื่อสินค้า</td>
-                      <td colspan="2">ยืนยันโดย (User)</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                    $sql_product = "SELECT * FROM orders,orders_type,product
-                    WHERE orders.orders_type_id = orders_type.orders_type_id AND orders.product_id = product.product_type_id AND orders.orders_type_id = '{$_REQUEST['o_id']}'";
-                    //echo $sql_product;
-                    $query_sql_product_type = mysqli_query($connect,$sql_product);
-                    while ($row = $query_sql_product_type->fetch_assoc()) { ?>
-                      <tr>
-                        <td><?php echo $row['orders_id']; ?></td>
-                        <td><?php echo $row['orders_type_name']; ?></td>
-                        <td><?php echo number_format($row['orders_qty']); ?> : ชิ้น</td>
-                        <td><?php echo number_format($row['orders_price']); ?></td>
-                        <td><?php @$date=mktime($row['orders_update']); echo date("d-m-Y",$date); ?></td>
-                        <td><?php echo $row['product_name']; ?></td>
-                        <td><?php echo $row['orders_user_submit']; ?></td>
-                        <td class="text-danger"><center><img src="../img/edit.png" width="30" /></center></td>
-                        <td class="text-danger"><center><img src="../img/cancel.png" width="30" /></center></td>
-                  </tr>
-                </tbody>
-                  <?php } ?>
-                </table>
+                    <td><?php echo $row['orders_id']; ?></td>
+                    <td><?php echo $row['product_name']; ?></td>
+                    <td><img class="rounded" src="../img/product/<?php echo $row['product_img'];?>" width="50px"></td>
+                    <td><?php echo number_format($row['orders_qty']); ?> : ชิ้น</td>
+                    <td><?php echo number_format($row['orders_price']); ?></td>
+                    <td><?php @$date=mktime($row['orders_update']); echo date("d-m-Y",$date); ?></td>
+                    <td><?php echo $row['order_user_submit']; ?></td>
+              </tr>
+            </tbody>
+              <?php } ?>
+            </table>
           </div>
         </div>
         <div class="card" style="width:100%; height: 30px; margin:0 0 10px 0;">
--- orders_type.php
+++ orders_type.php
@@ -21,29 +21,28 @@
                   <thead>
                   <tr>
                       <td>รหัสรายการสินค้า</td>
-                      <td>ประเภทรายการ</td>
+                      <td colspan="2">ชื่อสินค้า</td>
                       <td>จำนวนสินค้า</td>
                       <td>ราคาสินค้า</td>
                       <td>วันที่</td>
-                      <td>ชื่อสินค้า</td>
                       <td>ยืนยันโดย (User)</td>
                   </tr>
                 </thead>
                 <tbody>
                   <?php
                     $sql_product = "SELECT * FROM orders,orders_type,product
-                    WHERE orders.orders_type_id = orders_type.orders_type_id AND orders.product_id = product.product_type_id AND orders.orders_type_id = '{$_REQUEST['o_id']}'";
+                    WHERE orders.orders_type_id = orders_type.orders_type_id AND orders.product_id = product.product_id AND orders.orders_type_id = '{$_REQUEST['o_id']}'";
                     //echo $sql_product;
                     $query_sql_product_type = mysqli_query($connect,$sql_product);
                     while ($row = $query_sql_product_type->fetch_assoc()) { ?>
                       <tr>
                         <td><?php echo $row['orders_id']; ?></td>
-                        <td><?php echo $row['orders_type_name']; ?></td>
+                        <td><?php echo $row['product_name']; ?></td>
+                        <td><img class="rounded" src="img/product/<?php echo $row['product_img'];?>" width="50px"></td>
                         <td><?php echo number_format($row['orders_qty']); ?> : ชิ้น</td>
                         <td><?php echo number_format($row['orders_price']); ?></td>
                         <td><?php @$date=mktime($row['orders_update']); echo date("d-m-Y",$date); ?></td>
-                        <td><?php echo $row['product_name']; ?></td>
-                        <td><?php echo $row['orders_user_submit']; ?></td>
+                        <td><?php echo $row['order_user_submit']; ?></td>
                   </tr>
                 </tbody>
                   <?php } ?>
